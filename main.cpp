e576f935c8945d96662ffb530bbafb231513ce6a
Jose-Carlos-Jimenez <2996381970101@ingenieria.usac.edu.gt>
2020-03-25 15:28:00 -0600

    [UPDATE] Implementación del menú de usuario.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
+#include <ncurses.h>
 
 using namespace std;
 
+void printMenu()
+{
+    int heigth, width, start_y, start_x;
+    heigth = 23;
+    width = 78;
+    start_x = 0;
+    start_y = 0;
+    WINDOW *win = newwin(heigth, width, start_y, start_x);
+    refresh();
+    box(win,0,0);
+    mvwprintw(win, 1, 1, "Proyecto 1, Estructuras de datos, USAC.");
+    mvwprintw(win, 3, 1, "1. Jugar.");
+    mvwprintw(win, 4, 1, "2. Cargar archivo.");
+    mvwprintw(win, 5, 1, "3. Reportes.");
+    mvwprintw(win, 6, 1, "4. Salir.");
+    wrefresh(win);
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "Ingresa un dato.";
-    int n = 0;
-    cin >> n;
-    cout << "El dato seleccionado es " + to_string(n);
-    system("clear");
+    initscr();
+    printMenu();
+    bool play = true;
+    while(play)
+    {
+        noecho();
+        cbreak();
+        char option = getch();
+        if(option == '1')
+        {
+
+        }
+        else if( option == '2')
+        {
+
+        }
+        else if( option == '3')
+        {
+
+        }
+        else if( option == '4')
+        {
+            clear();
+            play = false;
+        }
+    }
+    endwin();
     return 0;
 }
